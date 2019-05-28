8fa18d2a2ef9caa3fa6d8559d2404b5ae9a146fe
Felix <google@ix5.org>
2019-05-28 14:05:51 +0200

    Use BOARD_VENDOR_SEPOLICY_DIRS
    BOARD_SEPOLICY_DIRS is deprecated.
    
    Change-Id: I046282b2a2e8c541726fb29cb0044503322d4be9
    Signed-off-by: Shivam Kumar <kumar.shivam.jarvis@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -175,7 +175,7 @@ include device/qcom/sepolicy/sepolicy.mk
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # Telephony
