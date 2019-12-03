f40fcdd900870ff48553abafb23739da26e1d703
Kyryll Bondarenko <rolfpeterkesche@googlemail.com>
2019-12-03 14:56:00 +0200

    Enable USB debugging at boot
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Signed-off-by: Shivam Kumar <kumar.shivam.jarvis@gmail.com>

--- device.mk
+++ device.mk
@@ -483,3 +483,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
+
+# USB debugging
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.debuggable=1
