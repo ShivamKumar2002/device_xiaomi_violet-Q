ShivamKumar2002/android_device_xiaomi_violet

Violet: Update Build Fingerprint and Description

init/init_violet.cpp
@@ -58,7 +58,7 @@ void property_override_dual(char const system_prop[], char const vendor_prop[],
 void vendor_load_properties()
 {
     // fingerprint
-   property_override("ro.build.description", "violet-user-10-QKQ1.190915.002-20.3.19-release-keys"); 
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ2A.200305.003/6156912:user/release-keys");
+   property_override("ro.build.description", "violet-user-10-QKQ1.190915.002-20.4.1-release-keys"); 
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/crosshatch/crosshatch:10/QQ2A.200405.005/6254899:user/release-keys");
     property_override("vendor.audio.adm.buffering.ms", "6");
 }

ShivamKumar2002/device_xiaomi_violet-Q

init/init_violet.cpp
@@ -58,6 +58,7 @@ void property_override_dual(char const system_prop[], char const vendor_prop[],
 void vendor_load_properties()
 {
     // fingerprint
-    property_override("ro.build.description", "violet-user 9 PKQ1.181203.001 V10.3.9.0.PFHINXM release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ1B.200105.004/6031802:user/release-keys");
+   property_override("ro.build.description", "violet-user-10-QKQ1.190915.002-20.4.1-release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ2A.200405.005/6254899:user/release-keys");
+    property_override("vendor.audio.adm.buffering.ms", "6");
 }