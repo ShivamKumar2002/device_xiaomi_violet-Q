afd06d5ab6871fefa460e89e8b9abb4ea9a0522d
Shivam Kumar <kumar.shivam.jarvis@gmail.com>
2020-05-03 08:50:18 +0000

    Use vulkan for ui rendering

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -121,7 +121,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_phase_offset_ns=500000 \
     debug.sf.early_app_phase_offset_ns=500000 \
     debug.sf.early_gl_phase_offset_ns=3000000 \
-    debug.sf.early_gl_app_phase_offset_ns=15000000
+    debug.sf.early_gl_app_phase_offset_ns=15000000 \
+    debug.hwui.renderer=skiavk
 
 #Camera
 PRODUCT_PROPERTY_OVERRIDES += \
