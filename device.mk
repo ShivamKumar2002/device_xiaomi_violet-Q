d3e5c18aa3e8b799f9931442ffac325377c2463c
Shivam Kumar <kumar.shivam.jarvis@gmail.com>
2020-05-04 09:16:51 +0000

    Call overlay-du

--- device.mk
+++ device.mk
@@ -24,7 +24,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-du
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
