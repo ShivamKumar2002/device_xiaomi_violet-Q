4d479688ce58fef3408ccb719146ebf3e84488b5
Shivam Kumar <kumar.shivam.jarvis@gmail.com>
2020-07-12 11:43:05 +0000

    Add some display props
    Signed-off-by: Shivam Kumar <kumar.shivam.jarvis@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -115,12 +115,15 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.protected_contents=true \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
+    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=1
 
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_gl_backpressure=1 \
     debug.sf.early_phase_offset_ns=500000 \
     debug.sf.early_app_phase_offset_ns=500000 \
+    debug.sf.disable_backpressure=1 \
+    debug.sf.hw=1 \
     debug.sf.early_gl_phase_offset_ns=3000000 \
     debug.sf.early_gl_app_phase_offset_ns=15000000 \
     debug.hwui.renderer=opengl
