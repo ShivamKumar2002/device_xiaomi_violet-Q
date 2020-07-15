ad69d0a5d0e3789baddbaf6bfdf330ac937e3b96
Shivam Kumar <kumar.shivam.jarvis@gmail.com>
2020-07-15 08:48:34 +0000

    Dont force hwc copy
    Signed-off-by: Shivam Kumar <kumar.shivam.jarvis@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -115,7 +115,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.protected_contents=true \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
-    ro.surface_flinger.force_hwc_copy_for_virtual_displays=1
 
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_gl_backpressure=1 \
