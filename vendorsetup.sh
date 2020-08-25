fb300f501eb0c74c7fb72ffb937a0d2d9cd25567
Shivam Kumar <Kumar.shivam.jarvis@gmail.com>
2020-08-25 19:00:51 +0530

    Fix compilation on jenkins

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+# Compilation with Prebuilt Kernel Fix
+cd vendor/du
+git fetch https://github.com/ShivamKumar2002/android_vendor_du
+git cherry-pick e313d495cf2aac829f59ca9b60b3937ea608c363
+cd ../../
